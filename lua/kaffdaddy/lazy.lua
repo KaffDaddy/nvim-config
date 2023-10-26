f77951b5ef8dc3be762c6f71ac936263b76db8c4
Stephan Salzmann <kaffdaddy@insomniaonline.de>
2023-10-26 21:46:56 +0200

    [TASK] Add colorscheme plugin

--- lua/kaffdaddy/lazy.lua
+++ lua/kaffdaddy/lazy.lua
@@ -11,4 +11,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({})
+require("lazy").setup(
+  { import = "kaffdaddy.plugins" },
+  {
+    install = {
+      colorscheme = { "nightfly" }
+    }
+  }
+)
