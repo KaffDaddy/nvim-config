99f71284dba1eca4ee2f5f0ae0af66943ba1b84c
Stephan Salzmann <kaffdaddy@insomniaonline.de>
2023-12-12 20:56:18 +0100

    [TASK] Add nvim-treesitter for parsing and highlighting

---
+++ lua/kaffdaddy/plugins/nvim-treesitter.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+
+      treesitter.setup({
+        highlight = {
+          enable = true,
+        },
+        indent = { enable = true },
+        autotag = {
+          enable = true,
+        },
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "yaml",
+          "html",
+          "css",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "php",
+          "scss",
+          "gitattributes",
+          "gitignore",
+          "json",
+          "json5",
+          "regex",
+          "typoscript",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+      })
+    end,
+  },
+}
