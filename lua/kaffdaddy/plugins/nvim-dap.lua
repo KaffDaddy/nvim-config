e435cd6f7730e4fbb7dbb94cd012bce55888281b
Stephan Salzmann <kaffdaddy@insomniaonline.de>
2023-12-30 18:05:40 +0100

    [TASK] Change path to vscode debugger in DAP configuration

--- lua/kaffdaddy/plugins/nvim-dap.lua
+++ lua/kaffdaddy/plugins/nvim-dap.lua
@@ -4,8 +4,21 @@ return {
     {
       "rcarriga/nvim-dap-ui",
       keys = {
-        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+        {
+          "<leader>du",
+          function()
+            require("dapui").toggle({})
+          end,
+          desc = "Dap UI",
+        },
+        {
+          "<leader>de",
+          function()
+            require("dapui").eval()
+          end,
+          desc = "Eval",
+          mode = { "n", "v" },
+        },
       },
       opts = {},
       config = function(_, opts)
@@ -47,52 +60,154 @@ return {
             -- all sources with no handler get passed here
 
             -- Keep original functionality
-            require('mason-nvim-dap').default_setup(config)
+            require("mason-nvim-dap").default_setup(config)
           end,
           php = function(config)
             config.adapters = {
-	            type = "executable",
-	            command = "node",
-              args = { '/Users/stephan/projects/vscode-php-debug/out/phpDebug.js' }
+              type = "executable",
+              command = "node",
+              args = { "~/projects/vscode-php-debug/out/phpDebug.js" },
             }
             config.configurations = {
               {
-                type = 'php',
-                request = 'launch',
+                type = "php",
+                request = "launch",
                 name = "Listen for XDebug",
                 port = 9003,
                 log = false,
                 pathMappings = {
-                  ['/app/'] = vim.fn.getcwd() .. '/',
+                  ["/app/"] = vim.fn.getcwd() .. "/",
                 },
-                hostname = '0.0.0.0',
-              }
+                hostname = "0.0.0.0",
+              },
             }
-            require('mason-nvim-dap').default_setup(config) -- don't forget this!
-        end,
+            require("mason-nvim-dap").default_setup(config) -- don't forget this!
+          end,
         },
         ensure_installed = {},
       },
     },
   },
   keys = {
-    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-    { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
-    { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
-    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
-    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
-    { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
-    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
-    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
-    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    {
+      "<leader>dB",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end,
+      desc = "Breakpoint Condition",
+    },
+    {
+      "<leader>db",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<leader>dc",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Continue",
+    },
+    {
+      "<leader>da",
+      function()
+        require("dap").continue({ before = get_args })
+      end,
+      desc = "Run with Args",
+    },
+    {
+      "<leader>dC",
+      function()
+        require("dap").run_to_cursor()
+      end,
+      desc = "Run to Cursor",
+    },
+    {
+      "<leader>dg",
+      function()
+        require("dap").goto_()
+      end,
+      desc = "Go to line (no execute)",
+    },
+    {
+      "<leader>di",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Step Into",
+    },
+    {
+      "<leader>dj",
+      function()
+        require("dap").down()
+      end,
+      desc = "Down",
+    },
+    {
+      "<leader>dk",
+      function()
+        require("dap").up()
+      end,
+      desc = "Up",
+    },
+    {
+      "<leader>dl",
+      function()
+        require("dap").run_last()
+      end,
+      desc = "Run Last",
+    },
+    {
+      "<leader>do",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
+    },
+    {
+      "<leader>dO",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+    {
+      "<leader>dp",
+      function()
+        require("dap").pause()
+      end,
+      desc = "Pause",
+    },
+    {
+      "<leader>dr",
+      function()
+        require("dap").repl.toggle()
+      end,
+      desc = "Toggle REPL",
+    },
+    {
+      "<leader>ds",
+      function()
+        require("dap").session()
+      end,
+      desc = "Session",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Terminate",
+    },
+    {
+      "<leader>dw",
+      function()
+        require("dap.ui.widgets").hover()
+      end,
+      desc = "Widgets",
+    },
   },
 
   config = function()
