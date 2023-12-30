1216a464e46fe783b989d9f20c4e6ce2d0281fb6
Stephan Salzmann <kaffdaddy@insomniaonline.de>
2023-12-30 18:02:30 +0100

    [TASK] Some option changes

--- lua/kaffdaddy/core/options.lua
+++ lua/kaffdaddy/core/options.lua
@@ -1,9 +1,10 @@
 local opt = vim.opt
 
 opt.number = true
+opt.relativenumber = true
 
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 
