c29b2608894d61aa3cbffee1d532ddec32564bef
Stephan Salzmann <kaffdaddy@insomniaonline.de>
2024-04-26 20:26:28 +0200

    [TASK] Add some basic options and add some dialects to treesitter

--- lua/kaffdaddy/core/options.lua
+++ lua/kaffdaddy/core/options.lua
@@ -6,11 +6,14 @@ opt.number = true
 opt.relativenumber = true
 opt.scrolloff = 8
 
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
-opt.autoindent = true
 
+opt.autoindent = true
 opt.smartindent = true
 opt.breakindent = true
 
@@ -31,6 +34,7 @@ opt.clipboard:append("unnamedplus")
 
 opt.splitright = true
 opt.splitbelow = true
+opt.splitkeep = "cursor"
 opt.colorcolumn = "120"
 
 opt.swapfile = false
